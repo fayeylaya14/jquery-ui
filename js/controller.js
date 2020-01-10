3fd422369836976f558252b170e5c1a14b6ff8e7
Faye Valerie L. Ylaya <49257189+fayeylaya14@users.noreply.github.com>
2020-01-10 10:42:15 +0800

    fixed already checkSort()

--- js/controller.js
+++ js/controller.js
@@ -2,26 +2,27 @@ $(document).ready(function(){
     $("#sort").sortable();
     $("#sort").disableSelection();
 
-        // var elemArr = document.getElementsByClassName("sort-ui");
-        // var arr = jQuery.makeArray(elemArr);
-        // arr = arr.map(data => data.innerHTML);
-        var arr = [];
 
-    function generateNum(){
-        var min=1;
-        var max=12;
-        return Math.floor(Math.random() * (+max - +min)) + +min;
-        // document.write("Random Number Generated : " + random );
-    }
+        function shuffleNum(myArr){
+            for (var i = myArr.length - 1; i > 0; i--) {
+                var j = Math.floor(Math.random() * (i + 1));
+                var temp = myArr[i];
+                myArr[i] = myArr[j];
+                myArr[j] = temp;
+            }
 
-    function storeToArray() {
+            return myArr;
+        }
+        var arr = [12,8,10,2,1,9,5,11,4,7,6,3];
+        var myarr = shuffleNum(arr);
+        console.log(arr);
+        storeToArray(arr);
+
+    function storeToArray(myarr) {
         var i = 0;
         var ul = document.getElementById("sort");
         var li;
         while(i < 12) {
-            var rec = generateNum();
-
-            arr[i] = rec;
             li = document.createElement("li");
             li.appendChild(document.createTextNode(arr[i]));
             ul.appendChild(li);
@@ -29,23 +30,12 @@ $(document).ready(function(){
         }
     }
 
-    storeToArray();
-
     $(".btn-check").on("click", function(){
         var elemArr = document.getElementsByTagName("li");
         var arrCheck = jQuery.makeArray(elemArr);
         arrCheck = arrCheck.map(data => data.innerHTML);
         var int;
         var len = arrCheck.length;
-        // for(int = 0; int < len-1; int++) {
-        //     if(arrCheck[int] > arrCheck[int+1]){
-        //         alert("This Group is Not Sorted");
-        //     } else if(arrCheck[int] == arrCheck[int+1]) {
-        //         alert("Congratulations!! This Group is Sorted");
-        //     } else {
-        //         alert("Congratulations!! This Group is Sorted");
-        //     }
-        // }
         var compare = isSorted(arrCheck);
         console.log(compare);
         if(compare == true) {
@@ -59,20 +49,15 @@ $(document).ready(function(){
     });
 
     function isSorted(arrCheck) {
-        var sortVal = 0;
+        var sortVal = true;
         var len = arrCheck.length;
         console.log(arrCheck);
         for(int = 0; int < len-1; int++) {
-            if(arrCheck[int] > arrCheck[int+1]){
+            if(parseInt(arrCheck[int]) > parseInt(arrCheck[int+1])) {
                 sortVal = false;
-            } else if(arrCheck[int] == arrCheck[int+1]) {
-                 sortVal = true;
-            }
-            else {
-                sortVal = true;
+                break;
             }
         }
-
         return sortVal;
     }
 });
